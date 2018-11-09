79bdce093a271a878f2ed2e9b3aee69d122650d4
SCE <sce@gglobaz.ch>
2018-11-09 11:16:06 +0100

    modify port

--- src/main/java/ch/sebooom/ApplicationWebXml.java
+++ src/main/java/ch/sebooom/ApplicationWebXml.java
@@ -15,7 +15,7 @@ public class ApplicationWebXml extends SpringBootServletInitializer {
         /**
          * set a default to use when no profile is configured.
          */
-        DefaultProfileUtil.addDefaultProfile(application.application());
+        //DefaultProfileUtil.addDefaultProfile(application.application());
         return application.sources(SebHipsterApp.class);
     }
 }
